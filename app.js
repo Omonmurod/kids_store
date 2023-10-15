0abe5db628a62917736022a99402e12f13bb7094
Omonmurod <omonmurod@gmail.com>
2023-10-15 14:44:00 +0900

    BRR: Initial setup

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+
+// MongoDB connect
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+// 1: Entering codes
+app.use(express.static("public")); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Codes based on SESSION
+
+// 3: Views code (BSSR backend side server rendering backendda viewni yasab olyapmiz)
+app.set("views", "views");
+app.set("view engine", "ejs"); 
+
+// 4: Rooting codes
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const mongodb = require("mongodb");
+
+const connectionString = process.env.MONGO_URL;
+mongodb.connect(
+  connectionString, 
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }, 
+  (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed!");
+      module.exports = client;  
+
+      const app = require("./app");
+      const server = http.createServer(app);  
+      let PORT = process.env.POT || 3013;
+      server.listen(PORT, function() {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
