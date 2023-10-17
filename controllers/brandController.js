f42c1fc5f81b2b3b3ad728ad46e45e4d6491eeba
Omonmurod <omonmurod@gmail.com>
2023-10-18 06:26:26 +0900

    feat: develop Product Controller logic

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,83 +1,96 @@
 const Member = require("../models/Member");
 
-let brandController = module.exports; /*pastdagi methodlarni yuklash imkonini beradi*/
-
+let brandController =
+  module.exports; /*pastdagi methodlarni yuklash imkonini beradi*/
 
 brandController.getMyBrandData = async (req, res) => {
   try {
-    console.log('GET: cont/getMyBrandData');
+    console.log("GET: cont/getMyBrandData");
     //TO DO: Get my brand products
-    
-    res.render('brand-menu');
-  } catch(err) {
+
+    res.render("brand-menu");
+  } catch (err) {
     console.log(`ERROR, cont/getMyBrandData, ${err.message}`);
-    res.json({state: 'fail', message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 brandController.getSignupMyBrand = async (req, res) => {
   try {
-    console.log('GET: cont/getSignupMyBrand');
-    res.render('signup'); /* Signup EJS ga boradi */
-  } catch(err) {
+    console.log("GET: cont/getSignupMyBrand");
+    res.render("signup"); /* Signup EJS ga boradi */
+  } catch (err) {
     console.log(`ERROR, cont/getSignupMyBrand, ${err.message}`);
-    res.json({state: 'fail', message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 brandController.signupProcess = async (req, res) => {
   try {
-    console.log('POST: cont/signup');
+    console.log("POST: cont/signup");
     const data = req.body,
-      member = new Member(),  /* 1-member object, 2-service model */
-      new_member = await member.signupData(data); /*ichiga req body yuborilyapti*/
+      member = new Member() /* 1-member object, 2-service model */,
+      new_member = await member.signupData(
+        data
+      ); /*ichiga req body yuborilyapti*/
 
-      req.session.member = new_member;
-      res.redirect('/brand/products/menu');
-    
-  } catch(err) {
+    req.session.member = new_member;
+    res.redirect("/brand/products/menu");
+  } catch (err) {
     console.log(`ERROR, cont/signup, ${err.message}`);
-    res.json({state: 'fail', message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 brandController.getLoginMyBrand = async (req, res) => {
   try {
-    console.log('GET: cont/getLoginMyBrand');
-    res.render('login-page'); /* Login EJS ga boradi */
-  } catch(err) {
+    console.log("GET: cont/getLoginMyBrand");
+    res.render("login-page"); /* Login EJS ga boradi */
+  } catch (err) {
     console.log(`ERROR, cont/getLoginMyBrand, ${err.message}`);
-    res.json({state: 'fail', message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 brandController.loginProcess = async (req, res) => {
   try {
-    console.log('POST: cont/login');
+    console.log("POST: cont/login");
     const data = req.body,
       member = new Member(),
       result = await member.loginData(data); /*ichiga req body yuborilyapti*/
 
-      req.session.member = result;
-      req.session.save(function() {
-        res.redirect('/brand/products/menu');
-      });
-
-  } catch(err) {
+    req.session.member = result;
+    req.session.save(function () {
+      res.redirect("/brand/products/menu");
+    });
+  } catch (err) {
     console.log(`ERROR, cont/login, ${err.message}`);
-    res.json({state: 'fail', message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 brandController.checkSessions = (req, res) => {
-  if(req.session?.member) {
-    res.json({ state: "succeed", data: req.session.member});
+  if (req.session?.member) {
+    res.json({ state: "succeed", data: req.session.member });
   } else {
-    res.json({ state: "failed", message: "You are not authenticated"});
+    res.json({ state: "failed", message: "You are not authenticated" });
   }
 };
 
 brandController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("You are Logged out");
+};
+
+brandController.validateAuthBrand = (req, res, next) => {
+  if (req.session?.member?.mb_type === "BRAND") {
+    /* Kelayotgan req ichida mb va mb type =BRAND bo'lsa*/
+    req.member = req.session.member;
+    /* Req member qismiga req sess mb yuklanadi */
+    next();
+  } else
+    res.json({
+      state: "fail",
+      message: "Only authenticated members with brand type",
+    });
 };
--- router_bssr.js
+++ router_bssr.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router_bssr = express.Router();
 const brandController = require("./controllers/brandController");
+const productController = require("./controllers/productController");
+const uploader_product = require("./utils/upload-multer")("products");
 
 /*****************************
  *          BSSR EJS          *
@@ -16,5 +18,12 @@ router_bssr.get("/logout", brandController.logout);
 router_bssr.get("/check-me", brandController.checkSessions);
 
 router_bssr.get("/products/menu", brandController.getMyBrandData);
+router_bssr.post(
+  "/products/create",
+  brandController.validateAuthBrand,
+  uploader_product.array("product_images", 5),
+  productController.addNewProduct
+);
+router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
 
 module.exports = router_bssr;
