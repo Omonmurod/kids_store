74ff69ec3cb35bb6f07de55b12012ef85ca43081
Omonmurod <omonmurod@gmail.com>
2024-01-03 19:00:32 +0900

    fix: app.js revised

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ app.use(express.static("public"));
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  credentials: true,
-  origin: true,
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(cookieParser());
 
 // 2: Codes based on SESSION
@@ -35,8 +37,8 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(function(req, res, next) {
-  res.locals.member = req.session.member;  
+app.use(function (req, res, next) {
+  res.locals.member = req.session.member;
   /* Har bir kelayotgan req browser ichiga yuborilyapti. 
   local ichidagi member objectda har bir authenticate bo'lgan user infosi bor */
   next();
@@ -60,24 +62,50 @@ const io = require("socket.io")(server, {
 });
 
 let online_users = 0;
-io.on('connection', function(socket){
+io.on("connection", function (socket) {
   online_users++;
   console.log("New user, total:", online_users);
   // socket.emit = for new commer
-  socket.emit('greetMsg', {text: 'Welcome'});
+  socket.emit("greetMsg", { text: "Welcome" });
   // io.emit = for all user
-  io.emit("infoMsg", {total: online_users});
+  io.emit("infoMsg", { total: online_users });
 
   socket.on("disconnect", function () {
     online_users--;
     // socket.broadcast.emit = to all users, except the left user
-    socket.broadcast.emit("infoMsg", {total: online_users});
+    socket.broadcast.emit("infoMsg", { total: online_users });
     console.log("Client dsconnected, total:", online_users);
-  }); 
+  });
 
-  socket.on('createMsg', function(data){
+  socket.on("createMsg", function (data) {
     console.log("createMsg:", data);
-    io.emit('newMsg', data);
+    io.emit("newMsg", data);
+  });
+
+  //  for chatting application
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
   });
 });
 
